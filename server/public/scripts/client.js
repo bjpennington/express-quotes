bdea6d4e7e5a8f0586c307e4aa41f3da87c1caad
BJ Pennington <bjpennington112@gmail.com>
2018-06-27 16:53:10 -0500

    connect client side with server side

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,14 @@ $(readyNow);
 
 function readyNow() {
     console.log('jq');
+    $.ajax({
+        url: '/quotes',
+        method: 'GET'
+    }).done(showQuotes).fail(function (errRes) {
+        alert('Request failed. Error: ' + errRes);
+    })
+}
+
+function showQuotes(response) {
+    console.log(response);
 }
