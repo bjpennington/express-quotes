3b57fd33c814d71cb3735869cdf206c8bfa4233b
BJ Pennington <bjpennington112@gmail.com>
2018-06-27 16:37:18 -0500

    created, exported, and required quotes array

---
+++ server/quotes.js
@@ -0,0 +1,19 @@
+let quotes = [];
+
+class Quote {
+    constructor(text, author) {
+        this.text = text;
+        this.author = author;
+    }
+}
+
+function addQuote(text, author) {
+    let newQuote = new Quote(text, author);
+    quotes.push(newQuote);
+}
+
+addQuote('Ain\'t no party like a Liz Lemon party, \'cause a Liz Lemon party is MANDATORY', 'Liz Lemon');
+addQuote('Separate the whites and the colors? What are you, some kind of laundry racist?', 'Bucky Katt');
+addQuote('Ya alright', 'Tho Kieu');
+
+module.exports = quotes;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+let quotes = require('./quotes')
 
 const app = express();
 const PORT = 5000;
 
 app.use(express.static('server/public'));
 
-app.listen(PORT, function() {
-    console.log('Listening on port:', PORT);  
+app.get('/quotes', function (req, res) {
+    res.send(quotes);
+});
+
+app.listen(PORT, function () {
+    console.log('Listening on port:', PORT);
 });
