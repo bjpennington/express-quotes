59c800bb963cc0067c50e72fa926b4b11a156ffc
BJ Pennington <bjpennington112@gmail.com>
2018-06-28 13:29:15 -0500

    established post on button click

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,13 +4,14 @@ $(readyNow);
 
 function readyNow() {
     console.log('jq');
+    $('#quoteBtn').on('click', addQuote);
     getQuotes();
 }
 
 function getQuotes() {
     $.ajax({
-        url: '/quotes',
-        method: 'GET'
+        url : '/quotes',
+        method : 'GET'
     }).done(showQuotes).fail(function (errRes) {
         alert('Request failed. Error: ' + errRes);
     })
@@ -23,4 +24,23 @@ function showQuotes(response) {
         let quoteString = `<li>\"${quoteText}\" — ${quoteAuthor}</li>`
         $('#quotesList').append(quoteString);
     }
+}
+
+function addQuote() {
+    let quoteText = $('#quoteText').val();
+    let quoteAuthor = $('#quoteAuthor').val();
+
+    $.ajax({
+        url : '/quotes',
+        method : 'POST',
+        data : {
+            text : quoteText,
+            author : quoteAuthor
+        }
+    }).done(function (response) {
+        console.log(response);
+    }).fail(function(errRes) {
+        alert('Request failed. Error: ' + errRes);
+    })
+
 }
